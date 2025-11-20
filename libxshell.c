0911eafe5d174ba62ce2b08994e1ddf3739f135f
John Doe <hanszimmer7171@proton.me>
2025-11-20 18:40:59 -0500

    Changed some things - v1.1.5-stable

--- libxshell.c
+++ libxshell.c
@@ -1,41 +1,38 @@
-#include <stdio.h>
 #include <limits.h>
-#include <string.h>
-#include <unistd.h>
 #include <pwd.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 #include "xsh.h"
 
 char cwd[PATH_MAX];
 
-
-// Information for the prompt (% if a regular user, or # if root)
 void prompt(){
-	char finalwd[PATH_MAX] = "";
-	uid_t uid = getuid();
-	struct passwd *pw = getpwuid(uid);
-	getcwd(cwd, sizeof(cwd));
-	if(strcmp(cwd, pw->pw_dir) == 0) {
-		char finalwd_home[PATH_MAX] = "~";
-		strcpy(finalwd, finalwd_home);
-	} else {
-		strcpy(finalwd, cwd);
-		char *finalwd_tail = strrchr(cwd, '/'); finalwd_tail++;
-		if(strcmp(finalwd_tail, "\0") == 0){
-			;
-		} else {
-			strcpy(finalwd, finalwd_tail);
-		}
-	}
-	if(strcmp(pw->pw_name, "root") != 0){
-		printf("%s %% ", finalwd);
-   		fflush(stdout);
-	} else {
-		printf("%s # ", finalwd);
-   		fflush(stdout);
-	}
+    char finalwd[PATH_MAX] = "";
+    uid_t uid = getuid();
+    struct passwd *pw = getpwuid(uid);
+    getcwd(cwd, sizeof(cwd));
+    if(strcmp(cwd, pw->pw_dir) == 0) {
+        char finalwd_home[PATH_MAX] = "~";
+        strcpy(finalwd, finalwd_home);
+    } else {
+        strcpy(finalwd, cwd);
+        char *finalwd_tail = strrchr(cwd, '/'); finalwd_tail++;
+        if(strcmp(finalwd_tail, "") == 0){
+            ;
+        } else {
+            strcpy(finalwd, finalwd_tail);
+        }
+    }
+    if(strcmp(pw->pw_name, "root") != 0){
+        printf("%s %% ", finalwd);
+           fflush(stdout);
+    } else {
+        printf("%s # ", finalwd);
+           fflush(stdout);
+    }
 }
-// When SIGINT occurs (Ctrl+C), just continue executing
 void handle_sigint(int sig){
-	printf("\n");
+    printf("\n");
     prompt();
 }

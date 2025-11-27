44f9bc2889b027af3024bd7065ef849941e72257
John Doe <hanszimmer7171@proton.me>
2025-11-27 11:01:02 -0500

    Moderate update - v1.4.5-stable

--- xsh.c
+++ xsh.c
@@ -31,7 +31,7 @@
 	char arch[15] = "other architecture";
 #endif
 
-char version[26] = "xsh version v1.4.0-stable";
+char version[26] = "xsh version v1.4.5-stable";
 char cwd[PATH_MAX];
 
 char disclaimer[1596] = "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
@@ -56,11 +56,18 @@ int main(int argc, char *argv[]){
 			printf("--version or -v: print version and exit\n");
 			printf("--license or -L: print license information and exit\n");
 			printf("--help or -h: display this help and exit\n");
+			printf("--command or -c: execute a single command\n");
 			return 0;
-		} else if(strcmp(argv[i], "-L") == 0 || strcmp(argv[i], "--license") == 0){
-			printf("Copyright (C) 2025, John Doe. License BSD 3-clause or later\n\nThis is free software. Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n\n1. Redistributions of source code must retain the above copyright notice, this lists of conditions, and the following disclaimer.\n\n2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n\n3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n\n");
-			printf("%s\n", disclaimer);
-			return 0;
+		} else if(strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--command") == 0){
+			if(argc >= 2){
+				char *x = argv[2];
+				cmdlogic(x);
+				return 0;
+			} else {
+				printf("xsh: %s requires an argument\n", argv[i]);
+				return 1;
+			}
+		}
 		} else {
 			int tryinter = interpret(argv[i]);
 			if(tryinter != 0){
